217c9b2af8ed6a5878370ec46a0b18a9c3a4b791
Marina-Dev <offrinmarina343@gmail.com>
2022-07-16 12:33:01 +0100

    Send data to firebase and add loading effect

--- firebase.js
+++ firebase.js
@@ -1,6 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApp, getApps } from "firebase/app";
 import {getStorage} from "firebase/storage"
+import {getFirestore} from "firebase/firestore"
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
